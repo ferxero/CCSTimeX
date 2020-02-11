368852684fe9cd956c483755318ddbcecfccaa0e
ferxero <dev.fer.x@gmail.com>
2020-02-10 21:05:16 -0400

    File init to event loop

---
+++ Sources/CCSTimeX/CCSProgressIndicator.swift
@@ -0,0 +1,25 @@
+//
+//  File.swift
+//  
+//
+//  Created by Fer Casillo on 2/9/20.
+//
+
+import SwiftUI
+
+@available(OSX 10.15, *)
+struct CCProgressIndicator: NSViewRepresentable {
+    @Binding var shouldAnimate:Bool
+    func makeNSView(context: Context) -> NSProgressIndicator {
+        let x=NSProgressIndicator()
+        x.style = .spinning
+        return x
+    }
+    func updateNSView(_ nsView: NSProgressIndicator, context: Context) {
+        if self.shouldAnimate {
+            nsView.startAnimation(self)
+        }else{
+            nsView.stopAnimation(self)
+        }
+    }
+}
---
+++ Sources/CCSTimeX/EventLoop.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  
+//
+//  Created by Fer Casillo on 2/9/20.
+//
+
+import Foundation
+
